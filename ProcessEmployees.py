ff359a8b9994359295e50e9067696a8cfd873e60
wylieholden <wylie_holden1@baylor.edu>
2023-02-13 15:09:38 -0600

    Quiz

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -1,4 +1,4 @@
-'''
+"""
 Due the outstanding performance of the Customer Service Represetatives (CSRs) in the marketing department, management
 has decided to reward them with a 10% increase in their salary. To evaluate the impact on the budget, you have been
 asked to run a report on the employee file and display the results of BEFORE AND AFTER the salary increase.
@@ -6,36 +6,43 @@ asked to run a report on the employee file and display the results of BEFORE AND
 You will create a dictionary that has the full employee name as the key and ONLY their new salary as the value.
 
 Iternate through the dictionary to print out their name and thier new salary (as in image)
-'''
+"""
 
 import csv
 
-#open the file
+# open the file
 
+CSVFILE = "employee_data.csv"
 
+# open
+infile = open(CSVFILE, "r", newline="")
+reader = csv.reader(infile)
+next(reader)
 
+# create an empty dictionary
+employee_salaries = {}
 
-#create an empty dictionary
-
-
-#use a loop to iterate through the csv file
-
-
-    #check if the employee fits the search criteria
-
-
+# use a loop to iterate through the csv file
+for employee in reader:
+    print(employee)
     
 
-print()
-print('=========================================')
-print()
 
-#iternate through the dictionary and print out the key and value as per printout
-
-
-
-          
+    # check if the employee fits the search criteria
+for employee in reader:
+    if (employee
         
+        employee[1] = {employee[5]}
 
-        
-    
+
+
+
+print(eq_dict)
+print()
+
+
+print()
+print("=========================================")
+print()
+
+# iternate through the dictionary and print out the key and value as per printout
